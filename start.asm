daafc2cf7ae121374ee849ce3877f977bec20f36
iyer <iyer@localhost.localdomain>
2020-04-22 01:18:18 -0400

    created the first .iso image successfully Signed-off-by: iyer <iyer@localhost.localdomain>

---
+++ start.asm
@@ -0,0 +1,23 @@
+bits 32
+global_start
+extern kernel_early
+extern main
+
+section .text
+	align 4
+	dd 0x1BADB002; magic
+	dd 0x00; flags
+	dd - (0x1BADB002 + 0x00);checksum
+_start:
+ cli
+ mov esp, stack
+ call kernel_early
+ call main
+ hlt
+
+section .bss
+resb 8192
+stack:
+
+
+
